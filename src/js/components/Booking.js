c071bb2110213840ecef3eca340526073799b149
Adam Kwidzyński <adamkwidzynski@gmail.com>
2022-07-11 20:13:27 +0200

    task 11.1 v1

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element){
@@ -20,6 +22,8 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
     thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
 
   }
 
@@ -28,6 +32,8 @@ class Booking {
     
     thisBooking.peopleAmountElem = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmountElem = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePickerElem = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPickerElem = new HourPicker(thisBooking.dom.hourPicker);
     
     thisBooking.dom.wrapper.addEventListener('updated', function(){});
   }
